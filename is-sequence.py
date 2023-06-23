3046c252c09f378ea6113bae8eec755245ebf8ac
Alexey <109007155+Alexey9911@users.noreply.github.com>
2023-06-22 21:09:17 -0500

    create link from leetcode exercise

--- is-sequence.py
+++ is-sequence.py
@@ -1,4 +1,6 @@
 '''
+link: https://leetcode.com/problems/is-subsequence/
+
 Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
 A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
@@ -25,6 +27,8 @@ class Solution:
             j += 1
 
         return True if i == len(s) else False
+    
+
 
 
 
