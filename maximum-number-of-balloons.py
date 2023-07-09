e27d2634c757427efc0aa80e03f60e66e19efaee
Alexey <109007155+Alexey9911@users.noreply.github.com>
2023-07-08 23:28:11 -0500

    maximum number of balloons

--- maximum-number-of-balloons.py
+++ maximum-number-of-balloons.py
@@ -22,3 +22,16 @@ Example 3:
 Input: text = "leetcode"
 Output: 0
 '''
+
+from collections import Counter
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        countText = Counter(text)
+        balloon = Counter("balloon")
+
+        res = len(text)  # or float("inf")
+        for c in balloon:
+            res = min(res, countText[c] // balloon[c])
+        return res
