abe6c084cf27c64335ced12909774ec0e42fc139
Alexey <109007155+Alexey9911@users.noreply.github.com>
2023-08-12 20:21:05 -0500

    added #3 solution

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -41,3 +41,15 @@ class Solution:
             i += 1 
         return s
         
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+        def reverse(l,r):
+            if l < r:
+
+                s[l],s[r] = s[r], s[l]
+                reverse(l + 1, r -1)
+        reverse(0,len(s) -1)
