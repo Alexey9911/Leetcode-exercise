d7b99fab29e034db2f95066fdfe9fc5743eff55a
Alexey <109007155+Alexey9911@users.noreply.github.com>
2023-08-26 20:02:18 -0500

    added exercise

---
+++ 682. Baseball Game.py
@@ -0,0 +1,27 @@
+# You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+# You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+# An integer x.
+# Record a new score of x.
+# '+'.
+# Record a new score that is the sum of the previous two scores.
+# 'D'.
+# Record a new score that is the double of the previous score.
+# 'C'.
+# Invalidate the previous score, removclass Solution:
+class Solution:
+    def calPoints(self, operations: list[str]) -> int:
+
+        stack = []
+
+        for i in operations:
+            if i == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif i == 'D':
+                stack.append(2 * stack[-1])
+            elif i == 'C':
+                stack.pop()
+            else:
+                stack.append(int(i))
+        return sum(stack)
